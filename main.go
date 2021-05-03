3b65d025e3b725862296cb786cff7ff9878c8a10
liuxiaosong <liuxiaosong@soyoung.com>
2021-05-03 10:55:54 +0800

    实现顺序栈

---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"src"
+)
+
+func main() {
+	stack := src.CreateArrayStack(12)
+}
---
+++ src/ArrayStack.go
@@ -0,0 +1,39 @@
+package src
+
+import "errors"
+
+// 顺序栈
+type arrayStack struct {
+	nodes []interface{}
+	len   int
+	count int
+}
+
+// 创建栈
+func CreateArrayStack(n int) *arrayStack {
+	return &arrayStack{
+		nodes: make([]interface{}, n),
+		len:   n,
+		count: 0,
+	}
+}
+
+// 出栈
+func (receiver *arrayStack) Push(value interface{}) error {
+	if (receiver.count + 1) == receiver.len {
+		return errors.New("full")
+	}
+	receiver.nodes[receiver.count] = value
+	receiver.count++
+	return nil
+}
+
+// 入栈
+func (receiver *arrayStack) Pull() (interface{}, error) {
+	if receiver.count == 0 {
+		return nil, errors.New("empty")
+	}
+	receiver.count--
+	value := receiver.nodes[receiver.count]
+	return value, nil
+}
